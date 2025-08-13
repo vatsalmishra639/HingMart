f46b4da56ebf2e455ee3db2ad05102211e5eb812
vatsalmishra639 <vatsalmishra639@gmail.com>
2025-08-13 12:13:12 +0530

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Hingmart/', // Replace with your GitHub repository name
+  base: './Hingmart/', 
 });
